b5979f3466f08719721e2163d29187f2a1c19ac0
Manuel Pineda <manuel.felipe.pineda@gmail.com>
2014-11-02 20:59:39 -0500

    Added utils and preparing the server for requests

--- src/server.cc
+++ src/server.cc
@@ -2,6 +2,8 @@
 #include <zmqpp/zmqpp.hpp>
 #include <dirent.h>
 
+#include "utils.cc"
+
 #define PIPELINE 10
 
 using namespace std;
@@ -10,7 +12,7 @@ using namespace zmqpp;
 const string music_path = "./music";
 
 
-bool searchFile(string name){
+bool search_file(string name){
   DIR* dirp = opendir(music_path.c_str());
   dirent* dp;
   if (dirp != NULL) {
@@ -37,34 +39,25 @@ void wake_up(socket &broker, string &address){
   cout << "Accepted by broker!" << endl;
 }
 
-
-void dispatch(message &incmsg, message &output) {
+/**
+ *  send_song : Must be called after a "fetch" request is performed.
+ *     Read from third frame : song_name
+ * */
+void send_song(message &request, message &response) {
   ifstream song;
-  // First frame in each message is the sender identity
-  string identity;
-  incmsg >> identity;
-
-  if (identity.size() == 0)
-    return; // Shutting down, quit
-
-  // Second frame is "fetch" command
-  string command;
-  incmsg >> command;
-  assert (command == "fetch");
-
   // Third frame is song name
   string song_name;
-  incmsg >> song_name;
+  request >> song_name;
 
-  if (searchFile(song_name)){
+  if (search_file(song_name)){
     song.open(music_path +  "/" + song_name);
     // Fourth frame is chunk offset in file
     size_t offset;
-    incmsg >> offset;
+    request >> offset;
 
     // Fifth frame is maximum chunk size
     size_t chunksz;
-    incmsg >> chunksz;
+    request >> chunksz;
 
     // Read chunk of data from file
     song.seekg(offset);
@@ -75,49 +68,79 @@ void dispatch(message &incmsg, message &output) {
     size_t size = song.gcount();
 
     string chunk(data, size);
-    output << identity << chunk;
+    response << chunk;
     song.close();
   } else {
-    output << identity << "NF";
+    response << "NF";
+  }
+}
+
+void search_song(message &request, message &response) {
+  response << "Not working yet";
+}
+
+
+void dispatch(message &incmsg, message &output) {
+
+  // First frame in each message is the sender identity
+  string identity;
+  incmsg >> identity;
+
+  if (identity.size() == 0)
+    return; // Shutting down, quit
+
+  // Second frame is "fetch" command
+  string command;
+  incmsg >> command;
+  if (command == "fetch") {
+    output << identity;
+    return send_song(incmsg, output);
+  }
+
+  if (command == "search") {
+    output << identity;
+    return search_song(incmsg, output);
   }
 
 }
 
 int main(int argc, char** argv) {
-  string song_endpoint = "tcp://*:";
-  string address = "tcp://";
+  string client_endpoint = "tcp://*:",
+         address = "tcp://";
+
   if (argc == 3) {
-    song_endpoint += argv[2];
-    address  += argv[1] + string(":") + argv[2];
+    int client_port = zen::ports::server_client + stoi(argv[2]);
+    client_endpoint += to_string(client_port);
+    address  += argv[1] + string(":") + to_string(client_port);
   } else {
-    cout << "Must provide an IP and port" << endl;
-    cout << "Usage : " << argv[0] << " ip port" << endl;
+    cout << "Must provide an IP and id" << endl;
+    cout << "Usage : " << argv[0] << " ip id" << endl;
     return 0;
   }
 
-  const string broker_endpoint = "tcp://localhost:6668";
-  context ctx;
+  const string broker_endpoint = "tcp://localhost:" + to_string(zen::ports::boker_server);
+  context context;
 
-  socket broker(ctx, socket_type::req);
+  socket broker(context, socket_type::req);
   broker.connect(broker_endpoint);
 
-  socket song_c(ctx, socket_type::router);
+  socket client(context, socket_type::router);
   // We have two parts per message so HWM is PIPELINE * 2
-  song_c.set(socket_option::send_high_water_mark, PIPELINE * 2);
-  song_c.bind(song_endpoint);
+  client.set(socket_option::send_high_water_mark, PIPELINE * 2);
+  client.bind(client_endpoint);
 
   wake_up(broker, address);
 
   poller poller;
-  poller.add(song_c);
+  poller.add(client);
 
   while (true) {
     if (poller.poll()) {
-      if (poller.has_input(song_c)) {
+      if (poller.has_input(client)) {
         message incmsg, output;
-        song_c.receive(incmsg);
+        client.receive(incmsg);
         dispatch(incmsg, output);
-        song_c.send(output);
+        client.send(output);
       }
     }
   }
---
+++ src/utils.cc
@@ -0,0 +1,8 @@
+
+namespace zen {
+  namespace ports {
+    int server_client = 5555; // Beginning of interval
+    int boker_server  = 6668;
+    int broker_client = 6667;
+  }
+}
